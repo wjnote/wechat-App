f93708c22c21ff3789cedec5a137b26b8bbee1c0
wjnote <641471534@qq.com>
2020-07-30 16:17:00 +0800

    首页的自定义组件

---
+++ pages/index/childCom/w-line/w-line.js
@@ -0,0 +1,26 @@
+// pages/index/childCom/w-text/w-text.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:{
+      type: String,
+      value: ''
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
